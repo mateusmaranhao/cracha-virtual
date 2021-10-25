27ed10451577d50cfaa02c14d71708a273f8f84f
Mateus <mateusmaranhao0210@gmail.com>
2021-10-25 14:03:06 -0300

    feat: consumir API do github

--- main.js
+++ main.js
@@ -8,9 +8,25 @@ const linksSocialMedia = {
 
 function changeSocialMediaLinks() {
   for(let li of socialLinks.children) {
-    const social = li.getAttribute('class');
-    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`;
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
   }
 }
 
 changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userImage.src = data.avatar_url;
+      userName.textContent = data.name;
+      userLogin.textContent = data.login;
+      userLink.href = data.html_url;
+      userBio.textContent = data.bio;
+    })
+}
+
+getGitHubProfileInfos()
